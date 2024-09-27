03527f563d8733b51eb4dd006fe9e9285544e6b9
Simon Rozman <simon@rozman.si>
2024-09-27 18:30:21 +0200

    Cleanup unmaintained project

--- src/pch.cpp
+++
@@ -1,6 +0,0 @@
-/*
-	SPDX-License-Identifier: MIT
-	Copyright © 2023-2024 Amebis
-*/
-
-#include "pch.hpp"
--- src/pch.hpp
+++
@@ -1,9 +0,0 @@
-/*
-	SPDX-License-Identifier: MIT
-	Copyright © 2023-2024 Amebis
-*/
-
-#pragma once
-
-#include "../include/MacStd/dyld.hpp"
-#include "../include/MacStd/unistd.hpp"
